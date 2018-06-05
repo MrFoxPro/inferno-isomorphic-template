e0785736f63b2bee3b6751e667b8125575ba492a
FoxPro <tuningiposadka@gmail.com>
2018-06-05 17:17:20 +0500

    Added devtools

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,11 @@
 import { Component, hydrate } from "inferno";
 import { BrowserRouter } from "inferno-router";
 import App from "./components/App/App";
-
+import { initDevTools } from "inferno-devtools";
 const wrapper = (
    <BrowserRouter>
       <App />
    </BrowserRouter>
 );
+initDevTools();
 hydrate(wrapper, document.getElementById("root"));
